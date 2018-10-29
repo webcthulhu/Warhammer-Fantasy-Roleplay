cb5fb9c91eadfb9d3911c754297fa38c46d4fa26
denis <denis.v.trocenko@gmail.com>
2018-10-29 12:37:22 +0200

    update: careers, skills, talents

--- src/app/shared/models/skill.ts
+++ src/app/shared/models/skill.ts
@@ -1,12 +1,12 @@
 export class Skill {
   name: string;
-  type: string;
+  advanced: string;
   grouped: boolean;
   characteristic: string;
   description: string;
   constructor(options) {
-    this.name = options.name || 'Unknown';
-    this.type = options.type || 'Unknown';
+    this.name = options.name || 'unknown';
+    this.advanced = options.advanced || false;
     this.grouped = options.grouped || false;
     this.characteristic = options.characteristic || '';
     this.description = options.description || 'No description provided yet.';
