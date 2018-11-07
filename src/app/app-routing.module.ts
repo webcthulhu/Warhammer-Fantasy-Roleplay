583381aab5d0c5ee5f0e7d9120a3e2823a2bae87
denis <denis.v.trocenko@gmail.com>
2018-11-07 13:51:03 +0200

    table module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   {path: 'main', loadChildren: 'src/app/main/main.module#MainModule'},
   {path: 'maps', loadChildren: 'src/app/maps/maps.module#MapsModule'},
   {path: 'rules', loadChildren: 'src/app/rules/rules.module#RulesModule'},
+  {path: 'table', loadChildren: 'src/app/table/table.module#TableModule'},
   {path: '', redirectTo: 'main', pathMatch: 'full'}
 ];
 
